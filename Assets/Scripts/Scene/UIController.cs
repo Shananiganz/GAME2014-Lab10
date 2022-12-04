d0cd35087aa68982f40f458103b0f218e90f75e4
Shananiganz <samantha.shanahan@georgebrown.ca>
2022-12-04 16:18:38 -0500

    Wired up the Y Button

--- Assets/Scripts/Scene/UIController.cs
+++ Assets/Scripts/Scene/UIController.cs
@@ -5,8 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
+    public GameObject miniMap;
+
+    private void Start()
+    {
+        miniMap = GameObject.Find("MiniMap");
+    }
+
     public void OnRestartButtonPressed()
     {
         SceneManager.LoadScene("Main");
+    }
+
+    public void OnYButtonPressed()
+    {
+        miniMap.SetActive(!miniMap.activeInHierarchy);
     }
 }
